7d2d5ff9845fb2f42e3b0edfec21164cb732818c
ankit-gautam23 <ankit.gautamp23@gmail.com>
2024-05-26 01:58:59 +0530

    added encrypt and decrypt files

--- decrypt.go
+++ decrypt.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "errors"
+    "io"
+)
+
+func decrypt(ciphertext []byte, key []byte, algorithm string) ([]byte, error) {
+    return nil, errors.New("decryption not implemented")
+}
--- encrypt.go
+++ encrypt.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+    "crypto/aes"
+    "crypto/cipher"
+    "crypto/rand"
+    "errors"
+    "io"
+)
+
+func encrypt(data []byte, key []byte, algorithm string) ([]byte, error) {
+    return nil, errors.New("encryption not implemented")
+}
