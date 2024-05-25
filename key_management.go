d4b43675e1c08ffaba34545ee3105f0c88476e70
ankit-gautam23 <ankit.gautamp23@gmail.com>
2024-05-26 01:57:02 +0530

    key management using cryptomator

--- key_management.go
+++ key_management.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"errors"
+	"github.com/cryptomator/cryptolib-go"
 )
 
 func generateKey(algorithm string) ([]byte, error) {
 	var key []byte
 	switch algorithm {
 	case "AES":
-		key = make([]byte, 32) 
+		key = make([]byte, 32) // 256-bit key for AES
 	case "RSA":
-		key = make([]byte, 2048) 
+		key = make([]byte, 2048) // 2048-bit key for RSA
 	case "ECC":
-		key = make([]byte, 32) 
+		key = make([]byte, 32) // 256-bit key for ECC
 	default:
 		return nil, errors.New("unsupported encryption algorithm")
 	}
 
+	// Generate random key
 	if _, err := rand.Read(key); err != nil {
 		return nil, err
 	}
@@ -25,6 +26,7 @@ func generateKey(algorithm string) ([]byte, error) {
 }
 
 func storeKey(key []byte, algorithm string, storagePath string) error {
+	// Store key in Cryptomator vault
 	vault, err := cryptolib.NewVault(storagePath)
 	if err != nil {
 		return err
@@ -39,6 +41,7 @@ func storeKey(key []byte, algorithm string, storagePath string) error {
 }
 
 func retrieveKey(algorithm string, storagePath string) ([]byte, error) {
+	// Retrieve key from Cryptomator vault
 	vault, err := cryptolib.NewVault(storagePath)
 	if err != nil {
 		return nil, err
@@ -53,11 +56,13 @@ func retrieveKey(algorithm string, storagePath string) ([]byte, error) {
 }
 
 func rotateKey(algorithm string, storagePath string) ([]byte, error) {
+	// Generate new key
 	newKey, err := generateKey(algorithm)
 	if err != nil {
 		return nil, err
 	}
 
+	// Store new key
 	err = storeKey(newKey, algorithm, storagePath)
 	if err != nil {
 		return nil, err
